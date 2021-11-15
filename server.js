7ca129460c566bcd1cc5e416ce16e76fd44768a8
Ruben Abel <rubenlage12@gmail.com>
2021-11-15 23:57:03 +0100

    Back

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
-// Cargar express forma parte de la Documentacion
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const app = express()
 
-//Para que express nos deje enlazar un css.
-//This code allows us to access all of the static files within the ‘public’ folder
-app.use(express.static('public')); 
-//Le digo que como sistema de plantillas elijo ejs.
-app.set('view engine', 'ejs') 
 
-// Express pasa a JSon, sustituye al body parser
-app.use(express.json())
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
-// app.get para hacer foco en un endpoint en concreto.
 app.get('/', function (req, res) {
-    // res.send('Hola mundo!!!!Servidor arriba.')
-    res.render('index')
+  res.render('index');
 })
-
-app.listen(3000, function () {
-    console.log('Example appp listening on port 3000!')
-})
-
-// Hacemos una Post request con el formulario
 app.post('/', function (req, res) {
-    res.render('index');
-    console.log(req.body.city);
-  })
+  console.log(req.body.city);
+  res.render('index');  
+})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
 
+
+///////////////////////////////////////////////////////////////////////
+const request = require('request');
+const apiKey = '9be547ad85aab170d6c84eb77fc4f4d2';
+//...
+//...
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
   
+request(url, function (err, res, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
